dd5f4bfd5edeeef9ffc6f5df61b09b62c3a75ac5
Wizmann <mail.kuuy@gmail.com>
2018-12-16 22:56:51 +0800

    add aes cipher

--- Melkweg/cipher.go
+++ Melkweg/cipher.go
@@ -1,6 +1,8 @@
 package Cipher
 
 import (
+    "crypto/aes"
+    "crypto/cipher"
     "crypto/md5"
     "fmt"
     "math/rand"
@@ -8,7 +10,8 @@ import (
 
 func DigestBytes(bytes []byte) []byte {
     h := md5.New()
-    return h.Sum(bytes)
+    h.Write(bytes)
+    return h.Sum(nil)
 }
 
 func DigestString(str string) []byte {
@@ -24,4 +27,37 @@ func Nonce(length int) []byte {
 
 func Hexlify(bytes []byte) string {
     return fmt.Sprintf("%x", bytes)
+}
+
+type ICipher interface {
+    Encrypt(buffer []byte) []byte
+    Decrypt(buffer []byte) []byte
+}
+
+type AESCipher struct {
+    iv []byte
+    stream cipher.Stream
+}
+
+func NewAESCipher(iv []byte, key string) *AESCipher {
+    block, err := aes.NewCipher(DigestString(key))
+    if (err != nil) {
+        panic(err)
+    }
+    return &AESCipher {
+        iv: iv,
+        stream: cipher.NewCTR(block, iv),
+    }
+}
+
+func (cipher *AESCipher) Encrypt(buffer []byte) []byte {
+    encrypted := make([]byte, len(buffer))
+    cipher.stream.XORKeyStream(encrypted, buffer)
+    return encrypted
+}
+
+func (cipher *AESCipher) Decrypt(buffer []byte) []byte {
+    decrypted := make([]byte, len(buffer))
+    cipher.stream.XORKeyStream(decrypted, buffer)
+    return decrypted
 }
--- Melkweg/cipher_test.go
+++ Melkweg/cipher_test.go
@@ -13,3 +13,23 @@ func TestDigest(t *testing.T) {
         t.Error()
     }
 }
+
+func TestAESCipher(t *testing.T) {
+    iv := DigestBytes(Nonce(100))
+    cipher1 := NewAESCipher(iv, "hello world")
+    cipher2 := NewAESCipher(iv, "hello world")
+
+    encrypted := cipher1.Encrypt([]byte("foo"))
+    decrypted := cipher2.Decrypt(encrypted)
+
+    if (bytes.Compare(decrypted, []byte("foo")) != 0) {
+        t.Error()
+    }
+
+    encrypted = cipher1.Encrypt([]byte("bar"))
+    decrypted = cipher2.Decrypt(encrypted)
+
+    if (bytes.Compare(decrypted, []byte("bar")) != 0) {
+        t.Error()
+    }
+}
