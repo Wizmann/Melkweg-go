a843980401c5efe3f5c7720ebb4024d457940cf3
Wizmann <mail.kuuy@gmail.com>
2018-12-17 19:03:55 +0800

    add protocol

--- src/Melkweg/protocol.go
+++ src/Melkweg/protocol.go
@@ -1,1 +1,17 @@
 package Melkweg
+
+type ProtocolState int;
+
+const (
+    READY   = ProtocolState(1)
+    RUNNING = ProtocolState(2)
+    DONE    = ProtocolState(3)
+    ERROR   = ProtocolState(4)
+)
+
+type Protocol struct {
+    key string
+    iv []byte
+    cipher ICipher
+    state ProtocolState
+}
